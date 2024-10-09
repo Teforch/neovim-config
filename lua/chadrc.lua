4c73e3d8f2ae1350c819ca2a6d1629a2248d3eea
Teforch <matveev.gleb.boy@gmail.com>
2024-10-09 11:57:18 +0300

    first commit

---
+++ lua/chadrc.lua
@@ -0,0 +1,48 @@
+-- This file needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+-- Please read that file to know all available options :(
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+  theme = "bearded-arc",
+  transparency = true,
+  statusline = {
+    theme = "vscode_colored",
+  },
+  nvdash = {
+    load_on_startup = true,
+    header = {
+      "██████╗ ██╗   ██╗██████╗ ██████╗ ██╗     ███████╗",
+      "██╔══██╗██║   ██║██╔══██╗██╔══██╗██║     ██╔════╝",
+      "██████╔╝██║   ██║██████╔╝██████╔╝██║     █████╗  ",
+      "██╔═══╝ ██║   ██║██╔══██╗██╔═══╝ ██║     ██╔══╝  ",
+      "██║     ╚██████╔╝██║  ██║██║     ███████╗███████╗",
+      "╚═╝      ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚══════╝╚══════╝",
+    },
+  },
+
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+    DiffChange = {
+      bg = "#464414",
+      fg = "none",
+    },
+    DiffAdd = {
+      bg = "#103507",
+      fg = "none",
+    },
+    DiffRemoved = {
+      bg = "#461414",
+      fg = "none",
+    },
+  },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+}
+
+return M
